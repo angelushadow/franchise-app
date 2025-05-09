92fb760be08835df202ef634bc244a9aa2db3df6
Angelus <angelushadow@gmail.com>
2025-05-09 06:55:35 -0500

    primer commit

---
+++ src/main/java/com/mickey/franchise/FranchiseAppApplication.java
@@ -0,0 +1,13 @@
+package com.mickey.franchise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranchiseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranchiseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mickey/franchise/FranchiseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mickey.franchise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchiseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
